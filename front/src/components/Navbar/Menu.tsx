6d1571dee81a118c33bb74cfb775fcebbc8d2d1a
Lisandro85 <lisandrobedotti@hotmail.com>
2025-02-18 09:21:10 +0100

    responsive navbar

--- front/src/components/Navbar/Menu.tsx
+++ front/src/components/Navbar/Menu.tsx
@@ -1,29 +1,78 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function Menu() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const router = usePathname();
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  return (
-    <div className="block lg:hidden">
-      <button onClick={toggleMenu} className="text-white">
-        <span className="block w-6 h-0.5 bg-black mb-1"></span>
-        <span className="block w-6 h-0.5 bg-black mb-1"></span>
-        <span className="block w-6 h-0.5 bg-black"></span>
-      </button>
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
 
-      {isMenuOpen && (
-        <div className="absolute top-5 left-0 bg-black text-white w-full p-4 shadow-md flex flex-col space-y-4">
-          <Link href={"/"}>Home</Link>
-          <Link href={"/about"}>About</Link>
-          <Link href={"/contact"}>Contact</Link>
-        </div>
-      )}
-    </div>
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <>
+      <div ref={menuRef} className="block  lg:hidden relative  ">
+        <button
+          onClick={toggleMenu}
+          className="text-white absolute top-0  p-4 bg-black w-full"
+        >
+          <span className="block w-6 h-0.5 bg-white mb-1"></span>
+          <span className="block w-6 h-0.5 bg-white mb-1"></span>
+          <span className="block w-6 h-0.5 bg-white "></span>
+        </button>
+
+        {isMenuOpen && (
+          <div className="absolute top-10   left-4 bg-black text-white w-auto p-4 shadow-md flex flex-col space-y-4">
+            <Link
+              href={"/"}
+              onClick={closeMenu}
+              className={`${
+                router === "/" ? "bg-amber-300" : ""
+              } hover:bg-yellow-500 p-2 rounded`}
+            >
+              Home
+            </Link>
+            <Link
+              href={"/about"}
+              onClick={closeMenu}
+              className={`${
+                router === "/about" ? "bg-amber-300" : ""
+              } hover:bg-yellow-500 p-2 rounded`}
+            >
+              About
+            </Link>
+            <Link
+              href={"/contact"}
+              onClick={closeMenu}
+              className={`${
+                router === "/contact" ? "bg-amber-300" : ""
+              } hover:bg-yellow-500 p-2 rounded`}
+            >
+              Contact
+            </Link>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
--- front/src/components/Navbar/Navbar.tsx
+++ front/src/components/Navbar/Navbar.tsx
@@ -2,16 +2,39 @@
 import Link from "next/link";
 import React, { useState } from "react";
 import Menu from "./Menu";
+import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const router = usePathname();
   return (
     <>
-      {/* Menú para pantallas grandes */}
       <div className="mx-auto justify-center bg-black text-white hidden lg:flex">
-        <nav className="space-x-4">
-          <Link href={"/"}>Home</Link>
-          <Link href={"/about"}>About</Link>
-          <Link href={"/contact"}>Contact</Link>
+        <nav className="space-x-4 m-2">
+          <Link
+            href={"/"}
+            className={`${
+              router === "/" ? "bg-amber-300" : ""
+            } hover:bg-yellow-500 p-2 rounded`}
+          >
+            Home
+          </Link>
+          <Link
+            href={"/about"}
+            className={`${
+              router === "/about" ? "bg-amber-300" : ""
+            } hover:bg-yellow-500 p-2 rounded`}
+          >
+            About
+          </Link>
+          <Link
+            href={"/contact"}
+            className={`${
+              router === "/contact" ? "bg-amber-300" : ""
+            } hover:bg-yellow-500 p-2 rounded`}
+          >
+            Contact
+          </Link>
         </nav>
       </div>
       <Menu />
